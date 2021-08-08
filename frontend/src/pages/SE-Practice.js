d066f1bd5ef3a7aea3338ab750d7fb2def6eb346
Kelvin Wong <kakoo40@gmail.com>
2021-08-09 11:08:58 +1200

    added submission form

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -2,20 +2,23 @@ import React from "react";
 import Styles from "../components/tablestyle.js";
 import Table from "../components/evidencetable.js";
 import tablecolumns from "../components/tablecolumns.js";
+import Dropdown from "../components/Dropdown.js";
 
+import articles from "../dummydata/articles.js";
 
 const SEPractice = () => {
-  return (
-    <div>
-      <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-             <Styles>
-               <Table
-                data={articles}
-                columns={tablecolumns}
-               />
-            </Styles>
-    </div>
-  );
+    return (
+      <div>
+        <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+              <Dropdown/>
+               <Styles>
+                 <Table
+                  data={articles}
+                  columns={tablecolumns}
+                 />
+              </Styles>
+      </div>
+    );
 }
 
 export default SEPractice;
